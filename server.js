5b4369a621aecb70be11a9df26770007e533938a
Marcos Wiendl <127610129+mwndl@users.noreply.github.com>
2023-10-10 10:06:33 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const app = express();
 
 app.get('/', (req, res) => {
   const ips = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  const clientIP = ips.split(',')[0].trim(); // Pega o primeiro IP da lista
-  res.send(`IP do Cliente: ${clientIP}`);
+  const clientIP = ips.split(',')[0].trim();
+  res.send(`Client IP: ${clientIP}`);
 });
 
 const port = 3000;
 app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
+  console.log(`Server running on port ${port}`);
 });
